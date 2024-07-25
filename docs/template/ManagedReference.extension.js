46dbf3b1a5ba975ae9bc1f50d0c862c4b35ff9b7
Shawn Tan <banchan@gmail.com>
2024-07-25 14:40:09 -0700

    add cjsha operator check, del mref.html.primary.js

--- docs/template/ManagedReference.extension.js
+++ docs/template/ManagedReference.extension.js
@@ -5,6 +5,43 @@
  * This method will be called at the start of exports.transform in ManagedReference.html.primary.js
  */
 exports.preTransform = function (model) {
+  if (model.inheritance){
+    let operatorType = {"source": false, "sink": false, "combinator": false};
+    inheritanceLength = model.inheritance.length;
+    for (let i = 0; i < inheritanceLength; i++){
+      if (model.inheritance[i].uid.includes('Bonsai.Source')){
+        operatorType.source = true;
+      }
+      else if (model.inheritance[i].uid.includes('Bonsai.Sink')) {
+        operatorType.sink = true;
+      }
+      else if (model.inheritance[i].uid.includes('Bonsai.Combinator')) {
+        operatorType.combinator = true;
+      }
+    }
+    if (!(operatorType.source || operatorType.sink || operatorType.combinator)){
+      if (model.syntax){
+        if (model.syntax.content[0].value.includes("[WorkflowElementCategory(ElementCategory.Source)]")){
+          operatorType.source = true;
+        }
+        else if (model.syntax.content[0].value.includes("[WorkflowElementCategory(ElementCategory.Sink)]")){
+          operatorType.sink = true;
+        }
+        else if (model.syntax.content[0].value.includes("[WorkflowElementCategory(ElementCategory.Combinator)]")){
+          operatorType.combinator = true;
+        }
+      } 
+    }
+    if (operatorType.source){
+      model.sourceNode = true;
+    }
+    else if (operatorType.sink){
+      model.sinkNode = true;
+    }
+    else if (operatorType.combinator){
+      model.combinatorNode = true;
+    }
+  }
   return model;
 }
 
